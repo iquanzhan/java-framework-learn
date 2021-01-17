7e882917d19a62736b4cf27bd24dbb01e99efabb
做全栈攻城狮 <iquanzhan@163.com>
2021-01-17 20:04:38 +0800

    update：CGLib动态代理实现

---
+++ 01设计模式专题/04代理模式/src/main/java/com/chengxiaoxiao/dynamic/cglib/Customer.java
@@ -0,0 +1,16 @@
+package com.chengxiaoxiao.dynamic.cglib;
+
+import com.chengxiaoxiao.statics.Person;
+
+/**
+ * 客户
+ *
+ * @Description:
+ * @Author: Cheng XiaoXiao  (🍊 ^_^ ^_^)
+ * @Date: 2021-01-17 17:37
+ */
+public class Customer implements Person {
+    public void findLove() {
+        System.out.println("高富帅");
+    }
+}
---
+++ 01设计模式专题/04代理模式/src/main/java/com/chengxiaoxiao/dynamic/cglib/JDKMeipo.java
@@ -0,0 +1,41 @@
+package com.chengxiaoxiao.dynamic.cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * JDKMeipo
+ *
+ * @Description:
+ * @Author: Cheng XiaoXiao  (🍊 ^_^ ^_^)
+ * @Date: 2021-01-17 17:33
+ */
+public class JDKMeipo implements MethodInterceptor {
+    private Object target;
+
+    public Object getInstance(Class clazz) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(clazz);
+        enhancer.setCallback(this);
+        return enhancer.create();
+    }
+
+
+    private void after() {
+        System.out.println("结束....");
+    }
+
+    private void before() {
+        System.out.println("开始....");
+    }
+
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        before();
+        Object obj = methodProxy.invokeSuper(o, objects);
+        after();
+        return obj;
+    }
+}
